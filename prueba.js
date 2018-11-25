3b5241444282148646e848158d24f0802dbf9081
Paula Ortiz <sarkofy@hotmail.com>
2018-11-25 02:37:35 -0600

    prueba

---
+++ prueba.js
@@ -0,0 +1,6 @@
+
+function centuryFromYear(year) {
+	                          //Math.ceil redondea hacia ariiba sin importar que el número no pase de .5
+return(Math.ceil(year/100)); //un siglo consta de 100 años
+}
+centuryFromYear();
